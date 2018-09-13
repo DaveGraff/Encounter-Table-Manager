47f7d787a6f2c93418cfd33a86b14a54ae66ca4e
Dave <daveagraff@gmail.com>
2018-09-13 18:07:23 -0400

    Fixed package names

--- Encounter Tables/src/encounter/tables/Table.java
+++ Encounter Tables/src/encounter/tables/Table.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package encounter.table;
+package encounter.tables;
 
 import java.util.Random;
 
